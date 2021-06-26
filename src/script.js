a0cd88893122f77dcb96ed1906f019463c82e45e
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-26 14:39:51 -0600

    Working Display

--- src/script.js
+++ src/script.js
@@ -1,77 +1,78 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import './style.css';
 
 const gltfLoader = new GLTFLoader();
 
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
-
+const scene = new THREE.Scene();
 
 //Phone
 
 gltfLoader.load('./CellPhone_IP6.gltf', (gltf) => {
-    
-    gltf.scene.scale.set(0.3, 0.3, 0.3)
-        gltf.scene.rotation.set(0, 4.8, 0);
+  gltf.scene.scale.set(0.3, 0.3, 0.3);
+  gltf.scene.rotation.set(0, 4.8, 0);
 
-    scene.add(gltf.scene)
+  scene.add(gltf.scene);
 
-    gui.add(gltf.scene.rotation,'x').min(-10).max(9)
-    gui.add(gltf.scene.rotation,'y').min(-10).max(9)
-    gui.add(gltf.scene.rotation,'z').min(-10).max(9)
+  gui.add(gltf.scene.rotation, 'x').min(-10).max(9);
+  gui.add(gltf.scene.rotation, 'y').min(-10).max(9);
+  gui.add(gltf.scene.translate, 'z').min(-10).max(9);
 });
 
 // Lights
 
-const pointLight = new THREE.AmbientLight(0xffffff, 1)
+const pointLight = new THREE.AmbientLight(0xffffff, 5);
 pointLight.position.y = 3;
-pointLight.position.x = 2
+pointLight.position.x = 2;
 
-pointLight.position.z = 4
-scene.add(pointLight)
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 1
-scene.add(camera)
-  
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 5;
+scene.add(camera);
+
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
@@ -80,34 +81,32 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+  alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
+  // Update objects
+  // sphere.rotation.y = .5 * elapsedTime
 
-    // Update objects
-    // sphere.rotation.y = .5 * elapsedTime
+  // Update Orbital Controls
+  // controls.update()
 
-    // Update Orbital Controls
-    // controls.update()
+  // Render
+  renderer.render(scene, camera);
 
-    // Render
-    renderer.render(scene, camera)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
