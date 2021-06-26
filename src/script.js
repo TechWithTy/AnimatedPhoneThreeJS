8f79a2ed1e7d626389e65ec9fa9ecc3db8ad13b4
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-26 13:55:09 -0600

    Phone Showing

--- src/script.js
+++ src/script.js
@@ -1,89 +1,77 @@
-import * as dat from 'dat.gui';
-import gsap from 'gsap';
-import * as THREE from 'three';
-import './style.css';
-//Texture Loader
-const textureLoader = new THREE.TextureLoader();
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const gltfLoader = new GLTFLoader();
+
 // Debug
-const gui = new dat.GUI();
+const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl');
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-const geometry = new THREE.PlaneBufferGeometry(1, 1.3);
 
-// Objects
-for (let i = 0; i < 4; i++) {
-  const material = new THREE.MeshBasicMaterial({
-    map: textureLoader.load(`/photographs/${i}.jfif`),
-  });
+//Phone
 
-  const img = new THREE.Mesh(geometry, material);
+gltfLoader.load('./CellPhone_IP6.gltf', (gltf) => {
+    
+    gltf.scene.scale.set(0.3, 0.3, 0.3)
+        gltf.scene.rotation.set(0, 5, 0);
 
-  img.position.set(Math.random() + 0.003, i * -1.8);
+    scene.add(gltf.scene)
 
-  scene.add(img);
-}
-
-let objs = [];
-
-scene.traverse((object) => {
-  if (object.isMesh) {
-    objs.push(object);
-  }
+    gui.add(gltf.scene.rotation,'x').min(-10).max(9)
+    gui.add(gltf.scene.rotation,'y').min(-10).max(9)
+    gui.add(gltf.scene.rotation,'z').min(-10).max(9)
 });
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.x = 2;
+const pointLight = new THREE.AmbientLight(0xffffff, 10)
 pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+pointLight.position.x = 2
+
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 2;
-scene.add(camera);
-
-gui.add(camera.position, 'y').min(-100).max(100);
-gui.add(camera.position, 'x').min(-100).max(100);
-
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 1
+scene.add(camera)
+  
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
@@ -92,75 +80,34 @@ gui.add(camera.position, 'x').min(-100).max(100);
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-//Moust
-window.addEventListener('wheel', onMouseWheel);
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-let y = 0;
-let position = 0;
-
-function onMouseWheel(event) {
-  y = -event.deltaY * 0.0005;
-}
-
-const mouse = new THREE.Vector2();
-
-window.addEventListener('mousemove', (event) => {
-  //mouse.x is the first vector which has access to x values
-  // of mouse divided by the total width of the browser screen(sizes.width).
-  mouse.x = (event.clientX / sizes.width) * 2 - 1;
-
-  //mouse.y is the second vector which has access to y values. we need to
-  // minus (-) the divided mouseY and screen size of Y to scroll down.
-  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
-});
 /**
  * Animate
  */
 
-const raycaster = new THREE.Raycaster();
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+const tick = () =>
+{
 
-  // Update objects
-  position += y;
-  y *= 0.9;
+    const elapsedTime = clock.getElapsedTime()
 
-  //Raycaster
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(objs);
-  console.log(intersects);
-  for (const intersect of intersects) {
-    gsap.to(intersect.object.scale, { x: 1.7, y: 1.7 });
-      gsap.to(intersect.object.rotation, { y: -0.5 });
-          gsap.to(intersect.object.position, { z: -0.9 });
+    // Update objects
+    // sphere.rotation.y = .5 * elapsedTime
 
-  }
+    // Update Orbital Controls
+    // controls.update()
 
-  for (const object of objs) {
-    if (!intersects.find((intersect) => intersect.object === object)) {
-      gsap.to(object.scale, { x: 1, y: 1 });
-        gsap.to(object.rotation, { y: 0 });
-              gsap.to(object.position, { z: 0 });
+    // Render
+    renderer.render(scene, camera)
 
-    }
-  }
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-  camera.position.y = -position;
-
-  // Update Orbital Controls
-  // controls.update()
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+tick()
